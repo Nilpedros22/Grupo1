c09f2a27d529df71d27b52a06601c55bf8930fae
Nil Pedrós Cabrillana <nilpedros@ceroca.cat>
2023-03-26 23:54:16 +0200

    Funciones php añadidas
    Funciones para el registro de usuarios y el inicio de sesión en la página añadidas.

--- login.php
+++ login.php
@@ -9,6 +9,24 @@
 </head>
 
 <body>
+    <?php
+    session_start();
+    if (isset($_POST['login'])) {
+        $conexion = mysqli_connect("localhost", "root", "") or die("Error al conectar a la bbdd");
+        $db = mysqli_select_db($conexion, "usuarios") or die("Error al conectar a la bbdd");
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $query = mysqli_query($conexion, "SELECT * FROM datos WHERE correo='" . $email . "' AND contraseña ='" . $password . "'");
+        $nr = mysqli_num_rows($query);
+        if ($nr == 1) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $email;
+            header("Location: inicio.php");
+        } else if ($nr == 0) {
+            echo "Correo o contraseña incorrectos.";
+        }
+    }
+    ?>
     <div>
         <h4 class="nuevo2">VIVE TU VIDA DE UNA MANERA DIFERENTE</h4>
         <h1 class="nuevo">ESTILO DE VIDA Y SALUD</h1>
@@ -22,7 +40,7 @@
         <img id="contraseña2" src="img/contraseña2.png">
         <input type="submit" value="Iniciar sesión" class="boton">
         <hr class="linea">
-        <input type="submit" value="Regístrate" class="boton-registro">
+        <input type="button" onclick="location.href = 'registro.php'" value="Regístrate" class="boton-registro">
         <h1 id="nuevo">Soy nuevo/a</h1>
     </div>
 </body>
